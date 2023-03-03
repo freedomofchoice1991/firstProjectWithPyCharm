45e11304e681caba0b996050e5c60240f7398b9b
Reza Karami <reza.karami.arbeiten@gmail.com>
2023-03-03 10:29:41 +0100

    Utility folder contents created!

---
+++ utilities/url_utilities.py
@@ -0,0 +1,9 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add the code needed to read the text file
+    pass
+
+def load_page(url: str):
+    #TODO add the code to read the url
+    pass
+
+
